f1e5bff305d377a81411f82442ea6955ae731767
SamiulH25 <194833810+SamiulH25@users.noreply.github.com>
2026-09-01 23:18:42 -0400

    chore: update gtfs.h

--- runtime/gtfs.h
+++ runtime/gtfs.h
@@ -1,2 +1,6 @@
 #pragma once
-void gtfs_init(const char *path);
+#include <stdint.h>
+#include <stddef.h>
+int gtfs_init(const char *iso_path);
+const char* gtfs_find(const char *name, uint32_t *out_off, uint32_t *out_size);
+size_t gtfs_read(const char *name, uint8_t **out);
